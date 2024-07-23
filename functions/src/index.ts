22c42e6c90f2603e600b6f8506168d3d9f600ce2
th4t-gi <juddbrau@gmail.com>
2024-07-22 21:57:44 -0600

    Created first cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,18 +7,33 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
+import { onDocumentCreated, onDocumentDeleted } from "Firebase-functions/v2/firestore"
 import * as logger from "firebase-functions/logger";
+import * as admin from "firebase-admin"
+import { getFirestore } from "firebase-admin/firestore"
+import { increment } from "firebase/firestore"
 
-
-import { initializeApp } from 'firebase-admin/app'
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-initializeApp()
+admin.initializeApp()
 
 export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
+  logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
+
+export const countyadded = onDocumentCreated("/users/{uid}/counties/{c}", (event) => {
+  const db = getFirestore()
+
+  db.doc(`stats/unique-visits`).set({
+    [event.params.c]: increment(1)
+  })
+})
+
+export const countydeleted = onDocumentDeleted("/users/{uid}/counties/{c}", (event) => {
+  const db = getFirestore()
+
+  db.doc(`stats/unique-visits`).set({
+    [event.params.c]: increment(1)
+  })
+})
+
